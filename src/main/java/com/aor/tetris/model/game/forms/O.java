b750af8eade2a2f589ea48a5fa1e94305e9b1b0a
davidsferreira02 <davidsferreira02@gmail.com>
2022-11-19 16:01:12 +0000

    Start develop game.Step1-forms

---
+++ src/main/java/com/aor/tetris/model/game/forms/O.java
@@ -0,0 +1,31 @@
+package com.aor.tetris.model.game.forms;
+
+import com.aor.tetris.model.Position;
+import com.aor.tetris.model.game.arena.Tetris;
+import com.aor.tetris.model.game.direction.Direction;
+
+public class O extends Tetris {
+
+    public O(Position position) {
+        super(position);
+        this.color = "CYAN";
+    }
+
+    public O(Tetris tetrimino) {
+        super(tetrimino.getCentralPosition());
+        this.color = "CYAN";
+    }
+
+    public Position[] getPositions(Direction direction) {
+        Position[] position = new Position[4];
+        switch (direction) {
+            case UP: case LEFT: case DOWN: case RIGHT:
+                position[0] = new Position(0, 0);
+                position[1] = new Position(-1, 0);
+                position[2] = new Position(0, -1);
+                position[3] = new Position(-1, -1);
+                break;
+        }
+        return position;
+    }
+}
---
+++ src/main/java/com/aor/tetris/model/game/forms/S.java
@@ -0,0 +1,50 @@
+package com.aor.tetris.model.game.forms;
+
+
+import com.aor.tetris.model.Position;
+import com.aor.tetris.model.game.arena.Tetris;
+import com.aor.tetris.model.game.direction.Direction;
+
+public class S extends Tetris {
+
+    public S(Position position) {
+        super(position);
+        this.color = "ORANGE";
+    }
+
+    public S(Tetris tetrimino) {
+        super(tetrimino.getCentralPosition());
+        this.color = "ORANGE";
+    }
+
+    public Position[] getPositions(Direction direction) {
+        Position[] position = new Position[4];
+        switch (direction) {
+            case UP:
+                position[0] = new Position(0, 0);
+                position[1] = new Position(0, -1);
+                position[2] = new Position(1, -1);
+                position[3] = new Position(-1, 0);
+                break;
+            case RIGHT:
+                position[0] = new Position(0, 0);
+                position[1] = new Position(0, -1);
+                position[2] = new Position(1, 0);
+                position[3] = new Position(1, 1);
+                break;
+            case DOWN:
+                position[0] = new Position(0, 0);
+                position[1] = new Position(0, 1);
+                position[2] = new Position(1, 0);
+                position[3] = new Position(-1, 1);
+                break;
+            case LEFT:
+                position[0] = new Position(0, 0);
+                position[1] = new Position(0, 1);
+                position[2] = new Position(-1, 0);
+                position[3] = new Position(-1, -1);
+                break;
+        }
+        return position;
+    }
+}
