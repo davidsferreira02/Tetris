8daec8bd43f2c6ea4b2ba4c4932dc516866a7ac8
davidsferreira02 <davidsferreira02@gmail.com>
2022-11-20 21:22:20 +0000

    More updates

--- src/main/java/com/aor/tetris/viewer/EndGame/EndGameViewer.java
+++ src/main/java/com/aor/tetris/viewer/EndGame/EndGameViewer.java
@@ -1,16 +1,15 @@
 package com.aor.tetris.viewer.EndGame;
 
+
+
 import com.aor.tetris.gui.GUI;
+import com.aor.tetris.model.Color;
 import com.aor.tetris.model.Position;
 import com.aor.tetris.model.game.EndGame.EndGame;
-import com.aor.tetris.model.game.arena.Arena;
-import com.aor.tetris.model.game.elements.Element;
 import com.aor.tetris.viewer.Viewer;
-import com.aor.tetris.viewer.game.ElementViewer;
-import com.aor.tetris.viewer.game.HeroViewer;
-import com.aor.tetris.viewer.game.MonsterViewer;
-import com.aor.tetris.viewer.game.WallViewer;
 
+import javax.imageio.IIOException;
+import java.io.IOException;
 import java.util.List;
 
 
@@ -18,24 +17,19 @@ import java.util.List;
         public EndGameViewer(EndGame endGame) {
             super(endGame);
         }
-/*
-        @Override
-        public void drawElements(GUI gui) {
-            drawElements(gui, getModel().getWalls(), new WallViewer());
-            drawElements(gui, getModel().getMonsters(), new MonsterViewer());
-            drawElement(gui, getModel().getHero(), new HeroViewer());
-
-            gui.drawText(new Position(0, 0), "Energy: " + getModel().getHero().getEnergy(), "#FFD700");
-        }
-
-        private <T extends Element> void drawElements(GUI gui, List<T> elements, ElementViewer<T> viewer) {
-            for (T element : elements)
-                drawElement(gui, element, viewer);
-        }
-
-        private <T extends Element> void drawElement(GUI gui, T element, ElementViewer<T> viewer) {
-            viewer.draw(element, gui);
-        }
-    }
-*/
+         public void drawElements(GUI gui){
+            Color color=new Color();
+            gui.drawText(new Position(5,5),"E",color.getColor("ORANGE"));
+             gui.drawText(new Position(6,5),"N",color.getColor("ORANGE"));
+             gui.drawText(new Position(7,5),"D",color.getColor("ORANGE"));
+             gui.drawText(new Position(8,5),"G",color.getColor("ORANGE"));
+             gui.drawText(new Position(9,5),"A",color.getColor("ORANGE"));
+             gui.drawText(new Position(10,5),"M",color.getColor("ORANGE"));
+             gui.drawText(new Position(11,5),"E",color.getColor("ORANGE"));
+             gui.drawText(new Position(7,13),getModel().getEntryName(),color.getColor("ORANGE"));
+         }
+         public void draw(GUI gui) throws IOException {
+            drawElements(gui);
+            gui.refresh();
+         }
     }
