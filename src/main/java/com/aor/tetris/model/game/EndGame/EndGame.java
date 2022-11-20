4b544998e1a4129ce85dc5e3132f913762558c9e
davidsferreira02 <davidsferreira02@gmail.com>
2022-11-20 10:35:26 +0000

    First things to implement a endgame mensage

---
+++ src/main/java/com/aor/tetris/model/game/EndGame/EndGame.java
@@ -0,0 +1,15 @@
+package com.aor.tetris.model.game.EndGame;
+
+public class EndGame {
+        private final String entry;
+
+        public EndGame(){
+            this.entry = "Exit";
+        }
+
+        public String getEntryName(){
+            return entry;
+        }
+    }
+
+
