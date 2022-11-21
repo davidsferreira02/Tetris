a743aeffb6f9078987f25f6922663e612c226226
davidsferreira02 <davidsferreira02@gmail.com>
2022-11-21 15:41:47 +0000

    feature almost done

--- src/main/java/com/aor/tetris/model/game/arena/Screen.java
+++ src/main/java/com/aor/tetris/model/game/arena/Screen.java
@@ -5,46 +5,54 @@ import com.aor.tetris.model.game.BlockRestrictions;
 import com.aor.tetris.model.game.elements.BlockHero;
 import com.aor.tetris.model.game.forms.Forms;
 import com.aor.tetris.model.game.forms.QueueForms;
+import com.aor.tetris.model.game.information.information;
 
 public class Screen {
-        private final int width;
-        private final int height;
+    private final int width;
+    private final int height;
+    private information information;
 
-        private QueueForms queue;
-        private Forms forms;
+    private QueueForms queue;
+    private Forms forms;
 
-        private BlockRestrictions blockRestrictions;
-
-
-
-        public Screen(int width, int height) {
-            this.width = width;
-            this.height = height;
-            this.queue = new QueueForms();
-            this.forms = queue.popNext();
+    private BlockRestrictions blockRestrictions;
 
 
-        }
-
-        public int getWidth() {
-            return width;
-        }
-
-        public int getHeight() {
-            return height;
-        }
-
-        public QueueForms getQueueForms() {
-            return queue;
-        }
+    public Screen(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.queue = new QueueForms();
+        this.forms = queue.popNext();
 
 
+    }
 
-        public Forms getForms() { return forms; }
+    public int getWidth() {
+        return width;
+    }
 
-        public void setForms(Forms fomrs) { this.forms = fomrs;}
+    public int getHeight() {
+        return height;
+    }
 
-        public BlockRestrictions getBlockRestrictions(){
-            return blockRestrictions;
-        }
+    public QueueForms getQueueForms() {
+        return queue;
+    }
+
+
+    public information getinformation() {
+        return information;
+    }
+
+    public Forms getForms() {
+        return forms;
+    }
+
+    public void setForms(Forms fomrs) {
+        this.forms = fomrs;
+    }
+
+    public BlockRestrictions getBlockRestrictions() {
+        return blockRestrictions;
+    }
 }
