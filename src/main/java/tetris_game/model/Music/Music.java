fe96f655c12839641d32453c6706dcb2371acde3
Guca08 <up202108839@up.pt>
2022-12-10 17:52:56 +0000

    music

---
+++ src/main/java/tetris_game/model/Music/Music.java
@@ -0,0 +1,50 @@
+package tetris_game.model.Music;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import java.io.File;
+import java.util.Objects;
+
+public class Music {
+    private final Clip music;
+    private static Music instance;
+
+    private Music() {
+        this.music = loadMusic();
+    }
+
+    public static Music getInstance(){
+        if (instance == null){
+            instance = new Music();
+        }
+        return instance;
+    }
+
+    private Clip loadMusic() throws NullPointerException {
+        try {
+            File musicFile = new File(Objects.requireNonNull(Music.class.getResource("/music/tetris.wav")).getFile());
+            AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicFile);
+            Clip musicClip = AudioSystem.getClip();
+            musicClip.open(audioInput);
+            FloatControl gainControl = (FloatControl) musicClip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-25.0f);
+            System.out.println(musicClip);
+            return musicClip;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void startMusic() {
+        music.setMicrosecondPosition(0);
+        music.start();
+        music.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stopMusic() {
+        music.stop();
+    }
+}
