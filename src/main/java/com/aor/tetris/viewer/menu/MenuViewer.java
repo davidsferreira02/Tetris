8b186a491503e956fb76920962422cf35f6490ce
Guca08 <up202108839@up.pt>
2022-12-10 15:23:40 +0000

    Menu changes

--- src/main/java/com/aor/tetris/viewer/menu/MenuViewer.java
+++ src/main/java/com/aor/tetris/viewer/menu/MenuViewer.java
@@ -15,16 +15,16 @@ public class MenuViewer extends Viewer<Menu> {
     public void drawElements(GUI gui) {
         Color color=new Color();
 
-        gui.drawText(new Position(7, 6), "T", color.getColor("RED"));
-        gui.drawText(new Position(8, 6), "E", color.getColor("ORANGE"));
-        gui.drawText(new Position(9, 6), "T", color.getColor("YELLOW"));
-        gui.drawText(new Position(10, 6), "R", color.getColor("GREEN"));
-        gui.drawText(new Position(11, 6), "I", color.getColor("BLUE"));
-        gui.drawText(new Position(12, 6), "S", color.getColor("PURPLE"));
+        gui.drawText(new Position(6, 6), "T", color.getColor("RED"));
+        gui.drawText(new Position(7, 6), "E", color.getColor("ORANGE"));
+        gui.drawText(new Position(8, 6), "T", color.getColor("YELLOW"));
+        gui.drawText(new Position(9, 6), "R", color.getColor("GREEN"));
+        gui.drawText(new Position(10, 6), "I", color.getColor("BLUE"));
+        gui.drawText(new Position(11, 6), "S", color.getColor("PURPLE"));
 
         for (int i = 0; i < getModel().getNumberEntries(); i++)
             gui.drawText(
-                    new Position(7, 9 + i),
+                    new Position(6, 9 + i),
                     getModel().getEntry(i),
                     getModel().isSelected(i) ? color.getColor("YELLOW") : color.getColor("WHITE"));
     }
