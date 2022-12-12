2a99fa3c70957a241f3414ee4f8fe146b0a10c40
David Ferreira <davidferreira@MacBook-Air-de-David.local>
2022-12-12 10:17:52 +0000

    More tests

---
+++ src/test/java/tetris_game/model/StatsTest.java
@@ -0,0 +1,32 @@
+package tetris_game.model;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class StatsTest {
+
+    Stats stats;
+
+    @BeforeEach
+    void setUp(){
+        stats = new Stats();
+    }
+
+    @Test
+    void testStats(){
+        Assertions.assertEquals(1, stats.getLevel());
+        stats.removedLines(4);
+        Assertions.assertEquals(80, stats.getPoints());
+        stats.removedLines(1);
+        stats.removedLines(3);
+        stats.removedLines(1);
+        stats.removedLines(1);
+        Assertions.assertEquals(10, stats.getLines());
+        Assertions.assertEquals(2, stats.getLevel());
+        Assertions.assertEquals(80 + 50 + 10*3, stats.getPoints());
+        stats.removedLines(2);
+        Assertions.assertEquals(80 + 50 + 30*2 + 10*3, stats.getPoints());
+    }
+}
